d537d29a98b5c1bc80e98e8fef7a488cf0678792
kevin <kevin_cao_91@163.com>
2019-04-13 14:27:31 +0800

    add res18, res34 model && GPU state

--- trainval_net.py
+++ trainval_net.py
@@ -238,6 +238,10 @@ if __name__ == '__main__':
     fasterRCNN = vgg16(imdb.classes, pretrained=True, class_agnostic=args.class_agnostic)
   elif args.net == 'res101':
     fasterRCNN = resnet(imdb.classes, 101, pretrained=True, class_agnostic=args.class_agnostic)
+  elif args.net == 'res18':
+    fasterRCNN = resnet(imdb.classes, 18, pretrained=True, class_agnostic=args.class_agnostic)
+  elif args.net == 'res34':
+    fasterRCNN = resnet(imdb.classes, 34, pretrained=True, class_agnostic=args.class_agnostic)
   elif args.net == 'res50':
     fasterRCNN = resnet(imdb.classes, 50, pretrained=True, class_agnostic=args.class_agnostic)
   elif args.net == 'res152':
@@ -308,10 +312,10 @@ if __name__ == '__main__':
     data_iter = iter(dataloader)
     for step in range(iters_per_epoch):
       data = next(data_iter)
-      im_data.data.resize_(data[0].size()).copy_(data[0])
-      im_info.data.resize_(data[1].size()).copy_(data[1])
-      gt_boxes.data.resize_(data[2].size()).copy_(data[2])
-      num_boxes.data.resize_(data[3].size()).copy_(data[3])
+      im_data.resize_(data[0].size()).copy_(data[0])
+      im_info.resize_(data[1].size()).copy_(data[1])
+      gt_boxes.resize_(data[2].size()).copy_(data[2])
+      num_boxes.resize_(data[3].size()).copy_(data[3])
 
       fasterRCNN.zero_grad()
       rois, cls_prob, bbox_pred, \
